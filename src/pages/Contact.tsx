91116d1b6371c6d31dd9f3e2919d44622bc48b43
essarebano <essarebano.dev@gmail.com>
2024-06-03 05:42:08 +0800

    Fixed styling of About, Contact, and Home pages

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,9 @@
 function Contact() {
   return (
-    <main className="p-4">
+    <section className="container mx-auto py-8">
       <h2 className="text-2xl font-bold mb-4">Contact Us</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non urna vitae nisl dapibus malesuada. Fusce eget nibh id libero volutpat auctor.</p>
-    </main>
+    </section>
   )
 }
 
