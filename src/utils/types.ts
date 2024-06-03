ea8fc2fcf04b800105ec61c8e341787dbf02febc
essarebano <essarebano.dev@gmail.com>
2024-06-03 11:33:54 +0800

    Updated types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,12 +1,38 @@
-export interface Recipe {
+export interface Special {
+  uuid: string;
+  ingredientId: string;
+  type: string;
   title: string;
-  image: string;
+  geo?: string;
+  text?: string;
+}
+
+export interface Ingredient {
+  uuid: string;
+  amount: number;
+  measurement: string;
+  name: string;
+}
+
+export interface Direction {
+  instructions: string;
+  optional: boolean;
+}
+
+export interface Recipe {
+  uuid: string;
+  title: string;
   description: string;
-  cookingTime: number;
-  prepTime: number;
+  images: {
+    full: string;
+    medium: string;
+    small: string;
+  };
   servings: number;
-  ingredients: string[];
-  directions: string[];
+  prepTime: number;
+  cookTime: number;
   postDate: string;
   editDate: string;
+  ingredients: Ingredient[];
+  directions: Direction[];
 }
