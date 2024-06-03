2458db16dc890da233b9699a2d08f39b0a35a31f
essarebano <essarebano.dev@gmail.com>
2024-06-03 08:23:07 +0800

    Updated Backdrop component shouldn't allow scrolling and implement it on Modal component

--- src/components/Backdrop.tsx
+++ src/components/Backdrop.tsx
@@ -1,15 +1,34 @@
+import { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 interface BackdropProps {
   isVisible: boolean;
-  onClick: () => void;
+  onClick?: () => void;
 }
+
+export const disableBodyScroll = () => {
+  document.body.style.overflow = 'hidden';
+};
+
+export const enableBodyScroll = () => {
+  document.body.style.overflow = '';
+};
 
 function Backdrop({
   isVisible,
   onClick
 }: BackdropProps) {
   if (!isVisible) return null;
+
+  useEffect(() => {
+    if (isVisible) {
+      disableBodyScroll()
+    } else {
+      enableBodyScroll()
+    }
+
+    return () => enableBodyScroll()
+  }, [isVisible])
 
   return ReactDOM.createPortal(
     <div
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom";
+import Backdrop from "./Backdrop";
 
 interface ModalProps {
   isOpen: boolean;
@@ -14,30 +15,33 @@ function Modal({
   if (!isOpen) return null;
 
   return ReactDOM.createPortal(
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white rounded-lg p-6 w-11/12 max-w-4xl relative">
-        <button
-          onClick={onClose}
-          className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 focus:outline-none"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+    <>
+      <Backdrop isVisible={isOpen} />
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+        <div className="bg-white rounded-lg p-6 w-11/12 max-w-4xl relative">
+          <button
+            onClick={onClose}
+            className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 focus:outline-none"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </button>
-        {children}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+          {children}
+        </div>
       </div>
-    </div>,
+    </>,
     document.getElementById('portal')!
   );
 };
